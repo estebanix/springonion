51e3598914bd4d18e285d81fdc8e10271aa926c9
estebanix <113446217+estebanix@users.noreply.github.com>
2023-06-06 11:28:46 +0200

    Update Recipes.js

--- src/Recipes.js
+++ src/Recipes.js
@@ -6,7 +6,7 @@ import RecipesContent from "./Files/RecipesContent";
 
 export default function Blogs(props){
     
-    function getShitDone(e){
+    function getItDone(e){
         props.handleClick(e)
     }
 
@@ -14,7 +14,7 @@ export default function Blogs(props){
         <main className="main--screen blogs">
             <RecipesHeader />
             <div className='blog-wave wave'></div>
-            <RecipesContent handleClick={getShitDone} />
+            <RecipesContent handleClick={getItDone} />
             <div className='footer-wave wave2'></div>
             <Footer />
         </main>   
