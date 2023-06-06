6ba067b7f010c340b0ddd526af6f2f64d5502f7b
estebanix <113446217+estebanix@users.noreply.github.com>
2023-06-06 11:21:20 +0200

    Update Hero.js

--- src/Files/Hero.js
+++ src/Files/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "./Nav";
-import img from 'C:/Users/dakis/Documents/VS/veterina/src/Images/hero-image.jpg';
+import img from '../src/Images/hero-image.jpg';
 
 export default function Hero(){
     const title = "Spring Onion"
